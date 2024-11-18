b517d61dd9584426fead0d98e8d1eddec62c9f09
Ahmed-W-Rabea <ahmedwael11425@gmail.com>
2024-11-18 17:37:22 +0200

    'the app bar of the home page '

--- lib/screens/home_page_cinema.dart
+++ lib/screens/home_page_cinema.dart
@@ -1,3 +1,4 @@
+import 'package:cinema_booking/cosntants/constants.dart';
 import 'package:flutter/material.dart';
 
 class HomePageCinema extends StatefulWidget {
@@ -10,6 +11,66 @@ class HomePageCinema extends StatefulWidget {
 class _HomePageCinemaState extends State<HomePageCinema> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: appBackgroundColor,
+      appBar: AppBar(
+        backgroundColor: appBackgroundColor,
+        title: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "Welcome Ahmed",
+                          style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                              letterSpacing: 1,
+                              color: Colors.white54),
+                        ),
+                        TextSpan(
+                          text: "👋",
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Let's relax and watch a movie!",
+                    style: TextStyle(
+                        height: 0.6,
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w900,
+                        letterSpacing: 1),
+                  )
+                ],
+              ),
+              Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: const DecorationImage(
+                        image: AssetImage("assets/images/ahmed.jpg"),
+                        fit: BoxFit.cover)),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
